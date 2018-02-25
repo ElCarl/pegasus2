f3f93a829208aacbf7290a3db51d114be7490fa5
Carl Heimann <carl.heimann@gmail.com>
2018-02-25 20:46:33 +0000

    Removed files that shouldn't be tracked (generated files etc.)

--- build/controller_interpreter/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pegasus/pegasus2/src/controller_interpreter
-# Build directory: /home/pegasus/pegasus2/build/controller_interpreter
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
