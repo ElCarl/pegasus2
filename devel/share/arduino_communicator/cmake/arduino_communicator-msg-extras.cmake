77aa89dc3cd45431ac9657459960361acc0aeb7e
Carl <carl.heimann@gmail.com>
2018-04-05 10:44:54 +0100

    Backup commit before .gitignore changes

--- devel/share/arduino_communicator/cmake/arduino_communicator-msg-extras.cmake
+++ devel/share/arduino_communicator/cmake/arduino_communicator-msg-extras.cmake
@@ -1,2 +1,2 @@
-set(arduino_communicator_MESSAGE_FILES "/home/sam/pegasus2/src/arduino_communicator/msg/EncoderCounts.msg")
+set(arduino_communicator_MESSAGE_FILES "/home/carl/pegasus2/src/arduino_communicator/msg/EncoderCounts.msg")
 set(arduino_communicator_SERVICE_FILES "")
