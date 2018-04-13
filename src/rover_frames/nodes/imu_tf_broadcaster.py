323a3faa3712decccdbe52921a9441495778aea3
Sam Price <samueljprice2305@gmail.com>
2018-04-13 15:45:42 +0100

    Added rover_frames package to handle transforms (not yet finished)

---
+++ src/rover_frames/nodes/imu_tf_broadcaster.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import rospy
+
+# to get commandline arguments
+import sys
+
+#Because of transformations
+import tf
+
+import tf2_ros
+import geometry_msgs.msg
+
+if __name__ == '__main__':
+  
+    rospy.init_node('imu_tf_broadcaster')
+    broadcaster = tf2_ros.StaticTransformBroadcaster()
+    static_transformStamped = geometry_msgs.msg.TransformStamped()
+  
+    static_transformStamped.header.stamp = rospy.Time.now()
+    static_transformStamped.header.frame_id = "base_link"
+    static_transformStamped.child_frame_id = "imu_link"
+  
+    static_transformStamped.transform.translation.x = 0.15
+    static_transformStamped.transform.translation.y = -0.08
+    static_transformStamped.transform.translation.z = 0.0
+
+    static_transformStamped.transform.rotation.x = 0.0
+    static_transformStamped.transform.rotation.y = 0.0
+    static_transformStamped.transform.rotation.z = 0.0
+    static_transformStamped.transform.rotation.w = 1.0
+ 
+    broadcaster.sendTransform(static_transformStamped)
+    rospy.spin()
---
+++ src/rover_frames/nodes/static_broadcaster_kinect.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import rospy
+
+# to get commandline arguments
+import sys
+
+#Because of transformations
+import tf
+
+import tf2_ros
+import geometry_msgs.msg
+
+if __name__ == '__main__':
+    rospy.init_node('kinect_static_broadcaster')
+    broadcaster = tf2_ros.StaticTransformBroadcaster()
+    static_transformStamped = geometry_msgs.msg.TransformStamped()
+  
+    static_transformStamped.header.stamp = rospy.Time.now()
+    static_transformStamped.header.frame_id = "base_link"
+    static_transformStamped.child_frame_id = "camera_link"
+  
+    static_transformStamped.transform.translation.x = 0.3
+    static_transformStamped.transform.translation.y = 0.0
+    static_transformStamped.transform.translation.z = -0.1
+
+    quat = tf.transformations.quaternion_from_euler(0.0, 0.0, 3.141592, 'rzyx')
+    static_transformStamped.transform.rotation.x = quat[0]
+    static_transformStamped.transform.rotation.y = quat[1]
+    static_transformStamped.transform.rotation.z = quat[2]
+    static_transformStamped.transform.rotation.w = quat[3]
+ 
+    broadcaster.sendTransform(static_transformStamped)
+    rospy.spin()
