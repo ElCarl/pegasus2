5e12ef8590b6be4d5f42bcbeceb7dd3cce4171c1
Carl <carl.heimann@gmail.com>
2018-04-13 14:28:14 +0100

    added some debug logging

--- src/arduino_communicator/scripts/arduino_communicator_node.py
+++ src/arduino_communicator/scripts/arduino_communicator_node.py
@@ -60,6 +60,7 @@ class RoverCommand:
         for the rover_target_vel topic
         """
         self.drive_command = vel_topic_data
+        rospy.logdebug("updated drive command")
 
     def update_arm_command(self, arm_topic_data):
         """
@@ -67,6 +68,7 @@ class RoverCommand:
         for the arm movement commands topic
         """
         self.arm_command = arm_topic_data
+        rospy.logdebug("updated arm command")
 
 
 class RoverController:
@@ -222,6 +224,7 @@ class BoardInterface:
         A command to control the camera servo will most likely be added
         at some point, probably another byte
         """
+        rospy.logdebug("sending commands: %s", " ".join(command_struct))
         self.serial_conn.write(BEGIN_MESSAGE_BYTE)
         command_data = struct.pack("<8B", *command_struct)
         # Send struct length - must convert len(command_data) to byte array
@@ -262,6 +265,7 @@ class BoardInterface:
         rec_checksum = encoder_data[-1]
         if rec_checksum == calc_checksum(data_str[:-1]):
             self.encoder_callback(encoder_data)
+            rospy.logdebug("encoder message received")
         else:
             rospy.logerr("Encoder checksum failure, ignoring message")
 
