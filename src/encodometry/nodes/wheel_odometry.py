c25b55512c56ee381d527d7a51f2974a7e4b03bc
Carl Heimann <carl.heimann@gmail.com>
2018-04-15 14:28:07 +0100

    encodometry node and tf stuff

--- src/encodometry/nodes/wheel_odometry.py
+++ src/encodometry/nodes/wheel_odometry.py
@@ -36,7 +36,7 @@ class odometry_node:
 		self.ticksPerMeter = int(rospy.get_param('~ticks_per_meter', 780))
         	self.wheelSeparation = float(rospy.get_param('~wheel_separation', 0.7))
         	self.rate = float(rospy.get_param('~rate', 10.0))
-        	self.baseFrameID = rospy.get_param('~base_frame_id', 'base_link')
+        	self.baseFrameID = rospy.get_param('~base_frame_id', 'base_footprint')
         	self.odomFrameID = rospy.get_param('~odom_frame_id', 'odom')
         	self.encoderMin = int(rospy.get_param('~encoder_min', -32768))
         	self.encoderMax = int(rospy.get_param('~encoder_max', 32767))
@@ -106,6 +106,7 @@ class odometry_node:
         	q.w = cos(self.pose.theta / 2)
        		self.tfPub.sendTransform((self.pose.x, self.pose.y, 0), (q.x, q.y, q.z, q.w),
 					now, self.baseFrameID, self.odomFrameID)
+
 	        odom = Odometry()
 	        odom.header.stamp = now
 	        odom.header.frame_id = self.odomFrameID
@@ -114,9 +115,22 @@ class odometry_node:
 	        odom.pose.pose.position.y = self.pose.y
 	        odom.pose.pose.position.z = 0
 	        odom.pose.pose.orientation = q
+		odom.pose.covariance[0] = 1e-3
+		odom.pose.covariance[7] = 1e-3
+		odom.pose.covariance[14] = 100000
+		odom.pose.covariance[21] = 100000
+		odom.pose.covariance[28] = 100000
+		odom.pose.covariance[35] = 1e-3
+
 	        odom.twist.twist.linear.x = self.pose.xVel
 	        odom.twist.twist.linear.y = 0
 	        odom.twist.twist.angular.z = self.pose.thetaVel
+		odom.twist.covariance[0] = 1e-3
+		odom.twist.covariance[7] = 1e-3
+		odom.twist.covariance[14] = 100000
+		odom.twist.covariance[21] = 100000
+		odom.twist.covariance[28] = 100000
+		odom.twist.covariance[35] = 1e-3
 		self.odomPub.publish(odom)
 
 	def __getitem__(self, item):
