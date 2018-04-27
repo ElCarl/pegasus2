ff8022a5b96664203c7613a100425502745f061b
= <carl.heimann@gmail.com>
2018-04-27 21:41:55 +0100

    changed loop for debugging

--- src/encoder_counter/src/encoder_counter/encoder_counter.ino
+++ src/encoder_counter/src/encoder_counter/encoder_counter.ino
@@ -1,13 +1,15 @@
+#include <SPI.h>
+
 // Input constants
 const uint8_t N_PINS = 14;
 const uint8_t N_ENCS = 7;   // This should be N_PINS / 2!
-const uint8_t INPUT_PINS[] = {2, 3, 4, 5, 6, 7, 8, 9, 15, 14, 17, 16, 19, 18};  // Order is important!
+const uint8_t INPUT_PINS[] = {2, 3, 4, 5, 6, 7, 8, 9, 15, 14, 17, 16, 19, 18};  // Order is important! It defines which pins belong to which encoder
 
 // Lookup table for encoder changes
 const int8_t LOOKUP_TABLE[] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
 
 // Serial communication constants
-const unsigned long BAUDRATE = 38400UL;
+const unsigned long BAUDRATE = 1000000;
 const byte REQUEST_ENCODER_COUNTS = 248;
 const byte SEND_MESSAGE           = 252;
 const byte END_MESSAGE            = 255;
@@ -17,6 +19,9 @@ uint8_t pin_states[]  = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 uint8_t enc_vals[]    = {0, 0, 0, 0, 0, 0, 0};
 long encoder_counts[] = {0, 0, 0, 0, 0, 0, 0};
 uint8_t state;
+
+// Debug globals
+uint16_t counter = 0;
 
 // Define the TX data structure
 struct ENCODER_DATA_STRUCTURE {
@@ -45,38 +50,42 @@ void setup() {
     pinMode(MISO, OUTPUT);
     pinMode(MOSI, INPUT);
     pinMode(SCK, INPUT);
-    pinMode(SS_PIN, INPUT);
-    
+    pinMode(SS, INPUT);
+
     // Set bits in the SPI register
     SPCR |= bit(SPE);   // Enable SPI in slave mode
     SPCR |= bit(SPIE);  // Enable SPI interrupts
-    
+
     // Begin serial connection: for debugging only
     Serial.begin(BAUDRATE);
+    Serial.println("Started");
 }
 
 // Interrupt service routine to handle SPI communications
 ISR (SPI_STC_vect) {
     // Read the data from the SPI register
-    uint8_t data = SPDR;
-    
+    volatile uint8_t data = SPDR;
+    Serial.print("SPDR: ");
+    Serial.println(data);
+
     // If encoder data is requested
     if (data == REQUEST_ENCODER_COUNTS) {
         // Load the data length into the SPI register
         SPDR = struct_len;
-        
+
         // Reset the transmission position, in case any communication
         // is lost from the master and it restarts
         transmission_pos = 0;
-        
+
         // Load the encoder data into the struct
-        for (uint8_t encoder = 0; encoder < N_ENCS; encoder++) {
+        for (volatile uint8_t encoder = 0; encoder < N_ENCS; encoder++) {
             encoder_counts_struct.encoder_counts[encoder] = encoder_counts[encoder];
         }
-        
-        // Calculate checksum
+
+        // Calculate checksum. This could go in the per-byte transmission if
+        // the setup in the first interrupt is too long
         checksum = struct_len;
-        for (uint8_t i = 0; i < struct_len; i++) {
+        for (volatile uint8_t i = 0; i < struct_len; i++) {
             checksum ^= *(struct_addr + i);
         }
     }
@@ -98,7 +107,7 @@ ISR (SPI_STC_vect) {
     }
 }
 
-void loop() {
+void loop_DISABLE() {
     for (uint8_t pin = 0; pin < N_PINS; pin++) {
         state = digitalRead(INPUT_PINS[pin]);
         // If the pin state has changed
@@ -109,6 +118,15 @@ void loop() {
             encoder_pin_change(pin / 2);  // (pin / 2) gives the encoder number:
         }                                 // 0 & 1 -> 0, 2 & 3 -> 1, etc.
     }
+}
+
+void loop() {// Debug loop
+    for (uint8_t i = 0; i < N_ENCS; i++) {
+        encoder_counts[i] = (counter + 1) * i;
+    }
+    counter++;
+    Serial.print("Count: "); Serial.println(counter);
+    delay(1000);
 }
 
 void encoder_pin_change(uint8_t encoder) {
